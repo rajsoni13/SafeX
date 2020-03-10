d0450fcc35d122452f2ee6b50eb9dfd836ba2f95
rajsoni13 <53208595+rajsoni13@users.noreply.github.com>
2020-03-10 10:25:25 +0530

    initial commit

---
+++ app/src/main/java/com/raj/safex/add_tp.java
@@ -0,0 +1,16 @@
+package com.raj.safex.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raj.safex.R;
+
+public class add_tp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tp);
+    }
+}
